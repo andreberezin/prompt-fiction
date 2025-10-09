0c36b8a673038be09429d3b09eec97210b5fed01
Andre Berezin <andreberezin@outlook.com>
2025-10-09 17:15:12 +0300

    added loading state

--- client/src/components/Output.tsx
+++ client/src/components/Output.tsx
@@ -5,17 +5,19 @@ import type OutputType from "../types/output.ts"
 interface OutputProps {
     output: OutputType;
     setOutput: React.Dispatch<React.SetStateAction<OutputType>>;
+    loadingState: boolean;
 }
 
-export default function Output({output, setOutput}: OutputProps) {
+export default function Output({output, setOutput, loadingState}: OutputProps) {
 
     return (
         <div
-            className='output'
+            className={`output`}
         >
 
             <textarea
                 id='output-text'
+                className={`${loadingState ? "loading" : ""}`}
                 value={output.content}
                 onChange={(e) =>
                     setOutput({...output, content: e.target.value })
