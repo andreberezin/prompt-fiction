117e79c5d20020be013a49aa599340489fe6ebbd
Andre Berezin <andreberezin@outlook.com>
2025-10-16 17:30:09 +0300

    edited output style, added options to display md and plain text

--- client/src/types/output.ts
+++ client/src/types/output.ts
@@ -1,6 +1,27 @@
-export default interface OutputType {
+export type Metadata = {
+    wordCount: number;
+    seoKeywords: Array<string>;
+    estimatedReadTime: string;
+}
+
+export type Section = {
+    type: string;
+    title: string;
     content: string;
+}
+
+export type ExportFormats = {
+    markdown: string;
+    plainText: string;
+    pdfReady: boolean;
+}
+
+export default interface OutputType {
     title?: string;
-    wordCount?: number;
-    keywords?: string[];
+    sections: Array<Section>;
+    metadata: Metadata;
+    exportFormats: ExportFormats;
+    content: string;
+    // wordCount?: number;
+    // keywords?: string[];
 }
