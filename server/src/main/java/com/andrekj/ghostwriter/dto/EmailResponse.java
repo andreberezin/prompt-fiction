319f183b7aac64171a309ba6fc40b43c1b66e578
Andre Berezin <andreberezin@outlook.com>
2025-10-16 17:29:46 +0300

    emailresponse class

--- server/src/main/java/com/andrekj/ghostwriter/dto/EmailResponse.java
+++ server/src/main/java/com/andrekj/ghostwriter/dto/EmailResponse.java
@@ -1,7 +1,18 @@
 package com.andrekj.ghostwriter.dto;
 
-import com.andrekj.ghostwriter.interfaces.BaseResponse;
+public class EmailResponse {
+    private String subject;
+    private String body;
+    private Metadata metadata;
+    private ExportFormats exportFormats;
 
-//public class EmailResponse implements BaseResponse {
-//
-//}
+    public static class Metadata {
+        private int wordCount;
+        private String estimatedReadTime;
+    }
+
+    public static class ExportFormats {
+        private String plainText;
+        private String richText;
+    }
+}
