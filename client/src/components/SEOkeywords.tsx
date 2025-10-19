ffe293d3be1bd0396bfca2b7a3886a97e3e68644
Andre Berezin <andreberezin@outlook.com>
2025-10-19 20:04:21 +0300

    added ability to see SEO keywords

---
+++ client/src/components/SEOkeywords.tsx
@@ -0,0 +1,16 @@
+import '../styles/keywords.scss'
+
+interface SEOkeywordsProps {
+    keywords?: string[];
+}
+
+
+export function SEOkeywords({ keywords }: SEOkeywordsProps) {
+    if (!keywords || keywords.length === 0) return null;
+
+    return (
+        <div id="seo-keywords">
+            <p>SEO keywords: {keywords.join(", ")}</p>
+        </div>
+    );
+}
