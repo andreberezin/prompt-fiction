35dfdba69fddf300f5b75f4c2ad6e4b0d21c0172
Andre Berezin <andreberezin@outlook.com>
2025-10-24 10:32:49 +0300

    added custom exception to handle content validation errors

---
+++ server/src/main/java/com/andrekj/ghostwriter/exceptions/ContentGenerationException.java
@@ -0,0 +1,7 @@
+package com.andrekj.ghostwriter.exceptions;
+
+public class ContentGenerationException extends RuntimeException {
+    public ContentGenerationException(String message) {
+        super(message);
+    }
+}
