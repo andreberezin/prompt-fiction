2cf08b910edb6ebd63b16edfe9cb7080756dfb14
Andre Berezin <andreberezin@outlook.com>
2025-10-24 10:34:05 +0300

    fixed pdf generation with plain text content

--- server/src/main/java/com/andrekj/ghostwriter/service/PDFGeneratorService.java
+++ server/src/main/java/com/andrekj/ghostwriter/service/PDFGeneratorService.java
@@ -6,57 +6,42 @@ import com.itextpdf.text.pdf.PdfWriter;
 import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayOutputStream;
-import java.util.List;
 
 @Service
 public class PDFGeneratorService {
 
-    // todo fix this: base it on the updated markdown content instead -> update the plain text content -> generate pdf
-    public void updateSections(BlogResponse editedResponse) {
+    // todo let the user choose font, alignment and so on
+public byte[] generateBlogPDF(BlogResponse blogResponse) {
+    try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+        Document document = new Document(PageSize.A4);
+        PdfWriter.getInstance(document, out);
+        document.open();
 
-        if (editedResponse.getExportFormats() != null
-                && editedResponse.getExportFormats().getPlainText() != null) {
+        // fonts
+        Font h1Font = new Font(Font.FontFamily.TIMES_ROMAN, 16, Font.BOLD);
+        Font h2Font = new Font(Font.FontFamily.TIMES_ROMAN, 14, Font.BOLD);
+        Font textFont = new Font(Font.FontFamily.TIMES_ROMAN, 12);
 
-            String[] editedSections = editedResponse.getExportFormats().getPlainText().split("\n\n");
-            List<BlogResponse.Section> sections = editedResponse.getSections();
+        Paragraph title = new Paragraph(blogResponse.getTitle(), h1Font);
+        title.setAlignment(Paragraph.ALIGN_CENTER);
+        document.add(title);
 
-            for (int i = 0; i < Math.min(sections.size(), editedSections.length); i++) {
-                sections.get(i).setPlainTextContent(editedSections[i].trim());
-            }
-        }
-    }
-
-
-    public byte[] generateBlogPDF(BlogResponse blogResponse) {
-        try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
-            Document document = new Document(PageSize.A4);
-            PdfWriter.getInstance(document, out);
-            document.open();
-
-            // fonts
-            Font h1Font = new Font(Font.FontFamily.TIMES_ROMAN, 16, Font.BOLD);
-            Font h2Font = new Font(Font.FontFamily.TIMES_ROMAN, 14, Font.BOLD);
-            Font textFont = new Font(Font.FontFamily.TIMES_ROMAN, 12);
-
-            Paragraph title = new Paragraph(blogResponse.getTitle(), h1Font);
-            title.setAlignment(Paragraph.ALIGN_CENTER);
-            document.add(title);
-
-            document.add(Chunk.NEWLINE);
+        document.add(Chunk.NEWLINE);
 
 //            int numOfSections = blogResponse.getSections().size();
 
-            for (BlogResponse.Section section : blogResponse.getSections()) {
-                document.add(new Paragraph(section.getTitle(), h2Font));
-                document.add(new Paragraph(section.getPlainTextContent(), textFont));
-                document.add(Chunk.NEWLINE);
-            }
+        for (BlogResponse.Section section : blogResponse.getSections()) {
 
-            document.close();
-            System.out.println("\n" + "\u001B[32m" +  "PDF Generated" + "\u001B[0m");
-            return out.toByteArray();
-        } catch (Exception e) {
-            throw new RuntimeException("Error generating PDF: " + e.getMessage());
+            document.add(new Paragraph(section.getTitle(), h2Font));
+            document.add(new Paragraph(section.getPlainTextContent(), textFont));
+            document.add(Chunk.NEWLINE);
         }
+
+        document.close();
+        //System.out.println("\n" + "\u001B[32m" +  "PDF Generated" + "\u001B[0m");
+        return out.toByteArray();
+    } catch (Exception e) {
+        throw new RuntimeException("Error generating PDF: " + e.getMessage());
     }
+}
 }
