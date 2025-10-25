58501af489b8341fc89a4929844fa0824c1e169a
Andre Berezin <andreberezin@outlook.com>
2025-10-26 01:11:59 +0300

    added stuff for the websockets

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,30 @@
 import './styles/App.scss'
 import NavBar from "./components/NavBar.tsx";
-import {useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import type {ContentType} from "./types/content.ts";
 import BlogForm from "./components/BlogForm.tsx";
 import EmailForm from "./components/EmailForm.tsx";
+import SocketHandler from "./sockets/SocketHandler.ts";
+import type {Client} from "@stomp/stompjs";
 
 function App() {
-    const [contentType, setContentType] = useState<ContentType>('blog')
+    const [contentType, setContentType] = useState<ContentType>('blog');
+    const [retryCounter, setRetryCounter] = useState<number>(0);
+    const [status, setStatus] = useState<string>("");
+    const stompClientRef = useRef<Client | null | undefined>(null);
+
+    // establish a websocket connection
+    useEffect(() => {
+        const socketHandler = new SocketHandler();
+        stompClientRef.current = socketHandler.createSocketConnection(setRetryCounter, setStatus);
+        return () => {stompClientRef.current?.deactivate()}
+    }, [])
+
 
   return (
     <div id={'ghostwriter'}>
         <NavBar contentType={contentType} setContentType={setContentType}/>
-        {contentType === 'blog' && <BlogForm/> }
+        {contentType === 'blog' && <BlogForm retryCounter={retryCounter} setRetryCounter={setRetryCounter} status={status} setStatus={setStatus}/> }
         {contentType === 'email' && <EmailForm/> }
     </div>
   )
