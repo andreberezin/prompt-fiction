115aba8e715c49c01bff89ee72e5724a46b32622
Andre Berezin <andreberezin@outlook.com>
2025-10-26 01:09:49 +0300

    cleanup

--- server/src/main/java/com/andrekj/ghostwriter/controller/ContentController.java
+++ server/src/main/java/com/andrekj/ghostwriter/controller/ContentController.java
@@ -2,16 +2,12 @@ package com.andrekj.ghostwriter.controller;
 
 import com.andrekj.ghostwriter.dto.BlogRequest;
 import com.andrekj.ghostwriter.dto.BlogResponse;
-import com.andrekj.ghostwriter.exceptions.AIServiceException;
 import com.andrekj.ghostwriter.service.BlogService;
 import com.andrekj.ghostwriter.service.PDFGeneratorService;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -31,9 +27,6 @@ public class ContentController {
 
     @PostMapping("/blog/generate")
     public BlogResponse generateBlogPost(@RequestBody BlogRequest request) {
-//        if (true) { // testing error handling
-//            throw new AIServiceException("Simulated AI API error");
-//        }
         return blogService.generateBlogPost(request, 1);
     }
 
@@ -42,20 +35,6 @@ public class ContentController {
         return blogService.updateBlogPost(editedResponse);
     }
 
-//    @PostMapping("/blog/pdf")
-//    public ResponseEntity<byte[]> generatePdf(@RequestBody BlogResponse editedResponse) {
-//        pdfGeneratorService.updateSections(editedResponse);
-//
-//        byte[] pdfBytes = pdfGeneratorService.generateBlogPDF(editedResponse);
-//
-//        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd:hh:mm:ss");
-//        String currentDateTime = dateFormatter.format(new Date());
-//
-//        return ResponseEntity.ok()
-//                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=Ghostwriter_blogPost_" + currentDateTime + ".pdf")
-//                .contentType(MediaType.APPLICATION_PDF)
-//                .body(pdfBytes);
-//    }
 
     @PostMapping("/blog/pdf")
     public ResponseEntity<byte[]> generatePdf(@RequestBody BlogResponse editedResponse) {
